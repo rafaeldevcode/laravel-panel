246cc87b69d2c15fb088e9ef269f752b5fce4fc6
Rafael Vieira <rafaeldevcode@gmail.com>
2022-12-15 07:05:49 -0300

    <refactor>[Messages]: Adicionando icone ao criar notificçõs no sistema

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,4 +1,5 @@
-<div class='position-fixed end-0 top-0 m-2 p-2 shadow border border-{{ $type }} border-2 rounded bg-cm-light' data-message='true'>
+<div class='d-flex flex-row align-items-center position-fixed end-0 top-0 m-2 p-2 shadow border border-{{ $type }} border-2 rounded bg-cm-light' data-message='true'>
+    <i class="bi {{ $type == 'danger' ? 'bi-x-circle-fill' : 'bi-check-circle-fill' }} text-{{ $type }} me-2"></i>
     <p class='m-0 text-{{ $type }}'>{{ $text }}</p>
 </div>
 
